016afc38e5b4fa7a6f2dca13c67ad0dafecb9d81
akash23-ai <akashbhardwaj2024@gmail.com>
2025-03-23 21:55:28 +0530

    contact hover effects

---
+++ src/lib/contact.ts
@@ -0,0 +1,25 @@
+
+
+
+export const contactItems = [
+    {
+      name : "linkedIn",
+      href : 'https://www.linkedin.com/in/akash-bhardwaj-b70a38161/',
+      imgsrc : 'https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png'
+    },
+    {
+      name : "Github",
+      href : 'https://github.com/akashbhardwaj23',
+      imgsrc : 'https://img.icons8.com/?size=100&id=63777&format=png&color=000000',
+    },
+    {
+      name : "twitter",
+      href : 'https://twitter.com/AkashBh2024?t=ZIVvM108UpB5PssuhFIT6g&s=09',
+      imgsrc : 'https://d33wubrfki0l68.cloudfront.net/ef67339f7016cb09ba66366c1dc9145ac69f2a21/feca1/assets/png/twitter-ico.png'
+    },
+    {
+      name : 'youtube',
+      href : '',
+      imgsrc : 'https://d33wubrfki0l68.cloudfront.net/493f1aa6da674c05bc606ba5b9e8092b881e8626/67c76/assets/png/yt-ico.png'
+    }
+  ]
